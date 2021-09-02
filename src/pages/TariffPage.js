ef5b3e4cbbd761cca04b129f0b795b77ccb4b3ae
SmithVyne <smithnkereuwem2@gmail.com>
2021-09-02 15:38:00 +0300

    tariff page spacing

--- src/pages/TariffPage.js
+++ src/pages/TariffPage.js
@@ -26,12 +26,18 @@ const SubTitle = styled.p`
     @media(max-width: 1000px) {
         font-size: 4%;
     }
+    @media(max-width: 450px) {
+        font-size: 5.5%;
+    }
 `
 const Div = styled.div`
     font-size: 100vw;
     display: flex;
     flex-direction: column;
     gap: 35px;
+    @media(max-width: 1000px) {
+        gap: 25px;
+    }
 `
 const Offers = styled.div`
     height: fit-content;
@@ -55,18 +61,18 @@ export default function TariffPage() {
                 <Offers>
                     <SubTitle>Для смартфонов для планшетов</SubTitle>
                     <Tariffs>
-                        <TariffCard scrolling title="Базовый" background="linear-gradient(135deg, #4B74FC 0%, #3039FF 100%)" icon={duck} />
-                        <TariffCard scrolling title="Яркий" background="linear-gradient(135deg, #4B5AFD 0%, #4B38FE 100%)" hit icon={star} />
-                        <TariffCard scrolling title="Расширенный" background="linear-gradient(135deg, #4B40FE 0%, #4B1EFF 100%);" icon={goblet} />
-                        <TariffCard scrolling title="Бизнес" background="radial-gradient(ellipse at center, #324E69 0%, #242424 100%)" icon={star} />
-                        <TariffCard scrolling title="VIP" background="radial-gradient(ellipse at center, #D79532 0%, #E1B470 50%, #1B240A 100%)" icon={goblet} />
+                        <TariffCard scrolling={true} title="Базовый" background="linear-gradient(135deg, #4B74FC 0%, #3039FF 100%)" icon={duck} />
+                        <TariffCard scrolling={true} title="Яркий" background="linear-gradient(135deg, #4B5AFD 0%, #4B38FE 100%)" hit icon={star} />
+                        <TariffCard scrolling={true} title="Расширенный" background="linear-gradient(135deg, #4B40FE 0%, #4B1EFF 100%);" icon={goblet} />
+                        <TariffCard scrolling={true} title="Бизнес" background="radial-gradient(ellipse at center, #324E69 0%, #242424 100%)" icon={star} />
+                        <TariffCard scrolling={true} title="VIP" background="radial-gradient(ellipse at center, #D79532 0%, #E1B470 50%, #1B240A 100%)" icon={goblet} />
                     </Tariffs>
                 </Offers>
                 <Offers>
                     <SubTitle>Для планшетов и модемов</SubTitle>
                     <Tariffs>
-                        <TariffCard scrolling title="Базовый" background="linear-gradient(135deg, #4B74FC 0%, #3039FF 100%)" icon={duck} />
-                        <TariffCard scrolling title="Яркий" background="linear-gradient(135deg, #4B5AFD 0%, #4B38FE 100%)" hit icon={star} />
+                        <TariffCard scrolling={true} title="Базовый" background="linear-gradient(135deg, #4B74FC 0%, #3039FF 100%)" icon={duck} />
+                        <TariffCard scrolling={true} title="Яркий" background="linear-gradient(135deg, #4B5AFD 0%, #4B38FE 100%)" hit icon={star} />
                     </Tariffs>
                 </Offers>
             </Div>
