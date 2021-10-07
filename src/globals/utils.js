babc3aaffeecda78a27a0d278487ca437c787008
Mikhail Surikov <miketogo@yandex.ru>
2021-10-08 00:27:32 +0300

    fix: tarif-info-page

--- src/globals/utils.js
+++ src/globals/utils.js
@@ -78,6 +78,8 @@ export const tariffBase = {
         gb: 10,
         sms: 100,
     }],
+    infinitInternet: 150,
+    modem: 50,
     price: 350,
     background:"linear-gradient(99.98deg, #4B74FC 0%, #3039FF 98.9%)"
 }
@@ -101,6 +103,8 @@ export const tariffBright = {
         gb: 25,
         sms: 500,
     }],
+    infinitInternet: 150,
+    modem: 50,
     price: 500,
     background:" linear-gradient(99.98deg, #4B40FE 0%, #3039FF 98.9%, #4B1EFF 98.9%);",
     hit: true,
@@ -126,6 +130,8 @@ export const tariffAdvanced = {
         gb: 35,
         sms: 1000,
     }],
+    infinitInternet: 'Включено в один из пакетов тарифа, для остальных пакетов 150 ₽',
+    modem: 50,
     price: 800,
     background:" linear-gradient(99.98deg, #4B40FE 0%, #3039FF 98.9%, #4B1EFF 98.9%)" 
 }
@@ -150,6 +156,8 @@ export const tariffBiz = {
         gb: 50,
         sms: 1000,
     }],
+    infinitInternet: 'Включено в один из пакетов тарифа, для остальных пакетов 150 ₽',
+    modem: 50,
     price: 1000,
     background:"radial-gradient(ellipse at center, #324E69 0%, #242424 100%)"
 }
@@ -168,6 +176,8 @@ export const tariffVip = {
         sms: 1000,
         internet: Infinity,
     }],
+    infinitInternet: 'Включено в тариф',
+    modem: 'Включено в тариф',
     price: 1500,
     background:"radial-gradient(ellipse at center, #D79532 0%, #E1B470 50%, #1B240A 100%)"
 }
